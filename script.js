baf91b7a83928fc082c33afc9f2b2519b62c9539
Curry <66020230+curryducker@users.noreply.github.com>
2024-08-18 22:30:31 +0200

    fix hamburger menu

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 function toggleNav() {
     document.querySelector('.nav-links').classList.toggle('mobile-hidden');
     document.querySelector('.hamburger-nav').classList.toggle('hamburger-active');
+
+    const links = document.querySelector('.nav-links').querySelectorAll('.link');
+    if (links[0].classList.contains('button-mobile-hidden')) {
+        links.forEach(element => {
+            element.classList.remove('button-mobile-hidden');
+        });
+    } else {
+        setTimeout(function() {
+            links.forEach(element => {
+                element.classList.add('button-mobile-hidden');
+            });
+        }, 350);
+    }
+
+    
 }
 
 function closeNav() {
     document.querySelector('.nav-links').classList.add('mobile-hidden');
     document.querySelector('.hamburger-nav').classList.add('hamburger-active');
+
+    setTimeout(function() {
+        const links = document.querySelector('.nav-links').querySelectorAll('.link');
+        links.forEach(element => {
+            element.classList.add('button-mobile-hidden');
+        });
+    }, 350);
 }
